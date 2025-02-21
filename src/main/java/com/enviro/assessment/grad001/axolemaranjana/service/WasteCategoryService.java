57ae15856767042698f90254eaedce020820ed21
Axole Maranjana <AxoleMaranjana4@gmail.com>
2025-02-21 10:19:21 +0200

    add: complete WasteCategoryService service

--- src/main/java/com/enviro/assessment/grad001/axolemaranjana/service/WasteCategoryService.java
+++ src/main/java/com/enviro/assessment/grad001/axolemaranjana/service/WasteCategoryService.java
@@ -1,4 +1,11 @@
 package com.enviro.assessment.grad001.axolemaranjana.service;
 
+import com.enviro.assessment.grad001.axolemaranjana.repository.WasteCategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class WasteCategoryService {
+    @Autowired
+    private WasteCategoryRepository wasteCategoryRepository;
 }
