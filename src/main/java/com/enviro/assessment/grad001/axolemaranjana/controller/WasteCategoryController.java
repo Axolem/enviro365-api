c219bda355ec265eb775ae8a26dd952231ee35dd
Axole Maranjana <AxoleMaranjana4@gmail.com>
2025-02-21 10:39:43 +0200

    feat: get all categories complete

--- src/main/java/com/enviro/assessment/grad001/axolemaranjana/controller/WasteCategoryController.java
+++ src/main/java/com/enviro/assessment/grad001/axolemaranjana/controller/WasteCategoryController.java
@@ -1,4 +1,26 @@
 package com.enviro.assessment.grad001.axolemaranjana.controller;
 
+import com.enviro.assessment.grad001.axolemaranjana.model.WasteCategory;
+import com.enviro.assessment.grad001.axolemaranjana.service.WasteCategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/waste-categories")
 public class WasteCategoryController {
+
+    @Autowired
+    WasteCategoryService categoryService;
+
+    @GetMapping("/")
+    public ResponseEntity<List<WasteCategory>> getAllCategories(){
+        return new ResponseEntity<>(categoryService.getAllCategories(), HttpStatus.OK);
+    }
+
 }
--- src/main/java/com/enviro/assessment/grad001/axolemaranjana/service/WasteCategoryService.java
+++ src/main/java/com/enviro/assessment/grad001/axolemaranjana/service/WasteCategoryService.java
@@ -1,11 +1,18 @@
 package com.enviro.assessment.grad001.axolemaranjana.service;
 
+import com.enviro.assessment.grad001.axolemaranjana.model.WasteCategory;
 import com.enviro.assessment.grad001.axolemaranjana.repository.WasteCategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class WasteCategoryService {
     @Autowired
     private WasteCategoryRepository wasteCategoryRepository;
+
+    public List<WasteCategory> getAllCategories(){
+        return wasteCategoryRepository.findAll();
+    }
 }
