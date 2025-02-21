c7610dc7405bb2e35b188421e13e060be1bddf28
Axole Maranjana <AxoleMaranjana4@gmail.com>
2025-02-21 10:13:22 +0200

    add: complete WasteCategory entity

--- src/main/java/com/enviro/assessment/grad001/axolemaranjana/model/WasteCategory.java
+++ src/main/java/com/enviro/assessment/grad001/axolemaranjana/model/WasteCategory.java
@@ -1,5 +1,47 @@
 package com.enviro.assessment.grad001.axolemaranjana.model;
 
-public class WasteCategory {
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
+
+@Entity(name = "waste_category")
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class WasteCategory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100, unique = true)
+    @NotNull(message = "'name' can not be null")
+    @NotBlank(message = "'name' is mandatory" )
+    @Size(min = 2, max = 100, message = "'name' must be between 2 and 100 characters")
+    private String name;
+
+    @Column(length = 500)
+    @NotNull(message = "'description' can not be null")
+    @NotBlank(message = "'description' is mandatory" )
+    @Size(min = 2, max = 100, message = "'description' must be between 2 and 500 characters")
+    private String description;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false, name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false, name = "updated_at")
+    private LocalDateTime updatedAt;
 }
